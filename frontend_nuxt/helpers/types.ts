2bcb410b912f029807f88ed3130d4abee69407d9
Alexander Gorbunov <algor.monte@gmail.com>
2024-07-12 21:17:36 +0200

    working chart

---
+++ frontend_nuxt/helpers/types.ts
@@ -0,0 +1,30 @@
+export interface ICrimeRecord {
+    dr_no: number;
+    date_rtpd: string;
+    date_occ: string;
+    time_occ: string;
+    area: number;
+    area_name: string;
+    rpt_dist_no: number;
+    part: number;
+    crm_cd: number;
+    crm_cd_desc: string;
+    moccodes: string;
+    vict_age: number;
+    vict_sex: string;
+    vict_descent: string;
+    premis_cd: number;
+    premis_desc: string;
+    weapon_used_co: number;
+    weapon_desc: string;
+    status: string;
+    status_desc: string;
+    crm_cd_1: number;
+    crm_cd_2: number;
+    crm_cd_3: number;
+    crm_cd_4: number;
+    location: string;
+    cross_street: string;
+    lat: number;
+    lon: number;
+}
--- frontend_nuxt/pages/LightningCharts.vue
+++ frontend_nuxt/pages/LightningCharts.vue
@@ -1,36 +1,50 @@
 <script setup lang="ts">
 import type {LightningChart} from "@arction/lcjs";
-import type {IMeta} from "~/helpers/types";
 
-const ls = inject<LightningChart>('lc');
+import type { ICrimeRecord } from "~/helpers/types";
 
-const crimeData = reactive<Record<string, unknown>[]>([]);
+const crimeData = reactive<ICrimeRecord[]>([]);
 async function getData() {
-  const { data } = await useFetch<Record<string, unknown>[]>('/api/crimes')
-  console.log(data.value)
+  const { data } = await useFetch<ICrimeRecord[]>('/api/crimes', {
+    method: 'post',
+    body: {
+      take: 10000,
+      skip: 0,
+    }
+  })
   if (data.value) {
     crimeData.splice(0, crimeData.length)
     crimeData.push(...data.value)
   }
 }
-
-const firstChart = ref<HTMLElement | null>(null);
-
 onBeforeMount(async () => {
   await getData();
+  const uniqueCrimePremises = new Set(crimeData.map(crime => crime.premis_desc));
+    chartPremisData.value = Array.from(uniqueCrimePremises).map(premis => {
+      return {
+        category: premis,
+        value: crimeData.filter(crime => crime.premis_desc === premis).length
+      }
+    })
 })
-onMounted(() => {
-  const barChart = ls?.BarChart()
-  if (firstChart.value && barChart) {
-
-  }
-});
+const ls = inject<LightningChart>('lc');
+const firstChart = ref<HTMLDivElement | null>(null);
+const chartPremisData = ref<{ category: string; value: number; }[]>([]);
+function createPremisChart() {
+ if (firstChart.value) {
+  const barChart = ls?.BarChart({
+      container: firstChart.value,
+    })
+  barChart?.setData(chartPremisData.value)
+ }
+}
+watch(() => chartPremisData.value, createPremisChart, { immediate: true })
 
 </script>
 
 <template>
 <div class="w-100 h-100">
-  <div ref="firstChart"></div>
+  <div class="w-100 h-100" ref="firstChart" />
 </div>
 </template>
 
